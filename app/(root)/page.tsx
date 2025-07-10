0aa218f6679357fd733cf7286b5aa8fe20f4c333
Saurav Punjabi <sauravpunjabi123@gmail.com>
2025-07-11 01:21:33 +0530

    made changes in homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
+import { Inter } from 'next/font/google'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import InterviewCard from "@/components/InterviewCard";
+
 
 const page = () => {
   return (
@@ -15,12 +20,38 @@ const page = () => {
           </p>
 
           <Button asChild className='btn-primary max-sm:w-full'>
-          <Link href='/interview'>Start an interview</Link>
-
+          <Link href='/interview'>
+            Start an interview
+          
+          </Link>
           </Button>
-
         </div>
 
+        <Image src= '/robot.png' alt = "robot" width={400} height={400} className='max-sm:hidden'/>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+          <h2>Your Interviews</h2>
+
+          <div className='interviews-section'>
+
+            {dummyInterviews.map((interview) =>(
+              <InterviewCard {...interview} key={interview.id}/>
+            ))}
+
+          </div>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+          <h2>Take an interview</h2>
+
+          <div className="interviews-section">
+            {dummyInterviews.map((interview) =>(
+              <InterviewCard {...interview} key={interview.id}/>
+            ))}
+
+            {/*<p>There are no interviews available</p>*/}
+          </div>
       </section>
     </>
   )
