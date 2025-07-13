382b3af51a7664336f6eaf90c108112a5f0b8db5
Saurav Punjabi <sauravpunjabi123@gmail.com>
2025-07-13 17:40:39 +0530

    interview ui

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,11 +1,20 @@
 import Image from 'next/image'
 import React from 'react'
 
-const Agent = () => {
+enum CallStatus{
+    Inactive = 'Inactive',
+    Connecting = 'Connecting',
+    Active = 'Active',
+    Done = 'Done'
+}
+
+const Agent = ({ userName } : AgentProps) => {
 
     const isSpeaking = true;
 
   return (
+
+   <> 
     <div className='call-view'>
       
         <div className='card-interviewer'>
@@ -16,10 +25,32 @@ const Agent = () => {
                 {isSpeaking && <span className='animate-speak' />}
 
             </div>
-
+                <h3>Interviewer</h3>
         </div>
 
+        <div className='card-border'>
+            <div className='card-content'>
+                <Image src = "/user-avatar.png" alt = "user avatar" width={540} height={540} className='rounded-full object-cover size-[120px]' />
+
+                <h3>{userName}</h3>
+            </div>
+        </div>
     </div>
+
+        <div className='w-full flex justify-center'>
+            {CallStatus !== 'Active' ? (
+                <button>
+                    <span>
+                        {CallStatus === 'Inactive' || CallStatus === 'Done' ? 'Call' : '...'}
+                    </span>
+                </button>
+            ) : (
+                <button className='btn-disconnect'>
+                    End
+                </button>
+            )}
+        </div>
+   </>
   )
 }
 
